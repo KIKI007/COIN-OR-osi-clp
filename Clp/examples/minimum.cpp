208cda548588d8aa4d8f926b2fece4ca4420f64b
rjodon <robin.jodon@epfl.ch>
2020-04-02 12:06:14 +0200

    Remove defined cond and use try/catch to open a file

--- Clp/examples/minimum.cpp
+++ Clp/examples/minimum.cpp
@@ -9,16 +9,15 @@ int main(int argc, const char *argv[])
      ClpSimplex  model;
      int status;
      if (argc < 2) {
-#if defined(SAMPLEDIR)
-/*
-  SAMPLEDIR should "path/to/Data/Sample"
-  Include the quotes and final path separator.
-*/
-          status = model.readMps(SAMPLEDIR "/p0033.mps", true);
-#else
-          fprintf(stderr, "Do not know where to find sample MPS files.\n");
-          exit(1);
-#endif
+
+
+         try {
+             status = model.readMps("p0033.mps", true);
+         } catch (const std::exception& e) {
+             fprintf(stderr, "No file p0033.mps found.\n");
+             exit(1);
+         }
+
      } else
           status = model.readMps(argv[1]);
      if (!status) {
